38286259a698086e43d58910c6ae2c9253719ae3
Ethan Zhang <ethanzhangyixuan@gmail.com>
2025-10-15 18:17:01 +1100

    successfully implemented database upload

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function askUpload(info) {
 
 async function upload(data) {
   try {
-    const res = await fetch("https://pacdex.vercel.app/api/upload", {
+    const res = await fetch("http://localhost:3000/api/upload", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
